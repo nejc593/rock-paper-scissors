6e928bcd317cc8adc362009eae97872ae9cf1cb0
Nejc <64842677+nejc593@users.noreply.github.com>
2021-01-28 12:10:42 +0100

    get computers choice

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ function promptPlayer() {
     return prompt("Do you choose rock, paper or scissors ?");
 
 }
+function computerChoice() {
+let randomNum = Math.floor(Math.random()*3);
+
+}
+
+
+
+
+
+
