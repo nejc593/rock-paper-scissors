94dc8d46590684fd3e0bb772cab43849cc76d94f
Nejc <64842677+nejc593@users.noreply.github.com>
2021-02-18 16:43:22 +0100

    created round function

--- script.js
+++ script.js
@@ -14,6 +14,19 @@ function computerChoice() {
     }
 }
 
+function round () {
+    if (playerChoice == computersChoice) {
+        console.log("It's a tie!");
+        return 0;
+    } else if ((playerChoice == "rock" && computersChoice == "paper") || (playerChoice == "paper" && computersChoice == "scissors") || (playerChoice == "scissors" && computersChoice == "rock")) {
+        console.log("You Lose! " + computersChoice + " beats " + playerChoice);
+        return -1;
+    } else {
+        console.log("You Win! " + playerChoice + " beats " + computersChoice);
+        return 1;
+    }
+
+}  
 
 
 
